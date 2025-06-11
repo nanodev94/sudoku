9bf4aa8ec50af0993d25bbef8fa1071d4c470c33
nanodev94 <nanodev94@gmail.com>
2025-06-11 17:19:33 +0200

    improve board style in mobile

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -16,15 +16,15 @@ const Board = ({
   onFieldClick,
 }: Props) => {
   return (
-    <div className='flex flex-col border-8 bg-white'>
+    <div className='flex flex-col border-4 sm:border-8 bg-white'>
       {gameBoard.map((row, rowIndex) => (
         <div
-          className='flex [&:nth-child(3n):nth-last-child(n+2)]:mb-2'
+          className='flex [&:nth-child(3n):nth-last-child(n+2)]:mb-1 sm:[&:nth-child(3n):nth-last-child(n+2)]:mb-2'
           key={`row-${rowIndex}`}
         >
           {row.map((fieldVal, colIndex) => (
             <Field
-              className='[&:nth-child(3n):nth-last-child(n+2)]:mr-2 outline-0'
+              className='[&:nth-child(3n):nth-last-child(n+2)]:mr-1 sm:[&:nth-child(3n):nth-last-child(n+2)]:mr-2'
               key={`field-(${rowIndex},${colIndex})`}
               row={rowIndex}
               col={colIndex}
